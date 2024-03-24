143df2708bba1c40c639a2018c69c24e26bd9a73
Diego Massarni <webdiegomassarini@gmail.com>
2024-03-25 09:48:53 +1000

    feat: add footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,51 @@
 import React from "react";
-
+import Image from "next/image";
 export default function Footer() {
-  return <div>Footer</div>;
+  const navigation = [
+    { name: "Tiramisu", href: "#tiramsisu" },
+    { name: "Behind the scene", href: "#behind" },
+    { name: "Testimonials", href: "#testimonials" },
+    { name: "Contact", href: "#contact" },
+  ];
+
+  return (
+    <footer className="m-4">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <Image
+              src="marti.svg"
+              width={60}
+              height={60}
+              className="h-10"
+              alt="Martimisu Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap font-gaegu uppercase">
+              Martimisu
+            </span>
+          </a>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
+            {navigation.map((item) => (
+              <li key={item.name}>
+                <a href={item.href} className="hover:underline me-4 md:me-6">
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © {new Date().getFullYear()}{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            MARTIMISU™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
 }
